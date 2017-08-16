24b26f5f36bdca29fd5866242bd5ebb6069fa429
a717822 <31062855+a717822@users.noreply.github.com>
2017-08-16 21:26:18 +0800

    - 优化代码

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
 var express = require('express');
 var app = express();
-var db = require("./db.js");
-var user = require('./Model/userController.js');
+var user = require('./Controller/UserController.js');
 var bodyParser = require('body-parser');
 // 创建 application/x-www-form-urlencoded 编码解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get('/index.html', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
-    // var data= {
-    //     username:'nodejs test',
-    //     realname:'nodejs'
-    // };
-    //
-    // for (var key1 in data) {
-    //     if (data.hasOwnProperty(key1)){
-    //         keyName.push(key1 + ' = ?');
-    //     }
-    //
-    //     modSqlParams.push(data[key1]);
-    // }
-    //
-    // keyString = keyName.join(',');
-    //
-    // db.database.db('admin').update(modSqlParams , keyString ,'id = 29' , function (data) {
-    //     if(data){
-    //         res.send(data);
-    //     }else{
-    //         res.send('更新失败');
-    //     }
-    //
-    // })
 });
 
 // 用户登录
@@ -41,7 +17,7 @@ app.post('/user_login', urlencodedParser , function (req, res) {
         password:req.body.password
     };
 
-    user.userController.user(response).login(function (data) {
+    user.userController(response).login(function (data) {
         res.json(data);
     });
 });
@@ -54,7 +30,7 @@ app.post('/user_register',urlencodedParser,function (req,res) {
         email:req.body.email
     };
 
-    user.userController.user(response).register(function (data) {
+    user.userController(response).register(function (data) {
         res.json(data);
     })
 
